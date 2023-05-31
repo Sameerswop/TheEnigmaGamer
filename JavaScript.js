9a4a8dd4baf33345947c6bc5728ed0db6bd4c884
Sameerswop <122268223+Sameerswop@users.noreply.github.com>
2023-05-31 21:30:58 +0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+function scrollToTop() {
+    window.scrollTo({
+       top: 0, /*The top parameter specifies the vertical position to scroll to, which is set to 0 to scroll to the top of the page.*/
+      behavior: "smooth" /*The behavior parameter determines the scrolling behavior. The value "smooth" makes the scrolling smooth and animated.*/
+  });
+}
